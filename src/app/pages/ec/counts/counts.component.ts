d32eb6372f46ad1ce71aa236776a3f0bc5f9a3a3
Dhruba Jyoti Dey <deydhrubajyoti@gmail.com>
2021-09-11 12:24:32 -0400

    Count Page Update
    Count Page Update

--- src/app/pages/ec/counts/counts.component.ts
+++ src/app/pages/ec/counts/counts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MemberDetailsService } from './../../../services/member-details.service';
+import { UserService } from './../../auth/core/user.service';
 
 import 'ag-grid-community';
 
@@ -16,90 +17,109 @@ export class CountsComponent implements OnInit {
   private gridApi;
   private gridColumnApi;
 
-  kkHeadCount:number;
+  kkHeadCount:number = 0;
 
-  nonHeadCount:number;
-  vegHeadCount:number;
-  kidHeadCount:number;
+  nonHeadCount:number =0;
+  vegHeadCount:number =0;
+  kidHeadCount:number =0;
 
-  
+  member:any;
+  lastOrder:boolean;
   //public modules: Module[] = [ClientSideRowModelModule];
 
   constructor( 
-    private mds: MemberDetailsService
+    private mds: MemberDetailsService,
+    public userService: UserService,
   ) {
-    this.mds.allPaypalPayments().subscribe(p => {
-      this.payments = p;
-      console.log(this.payments);
-      this.rowData = this.payments;
-      this.kkShowTicketCount();
-    });
 
    }
 
   ngOnInit(): void {
+    this.mds.allMembersDetails().subscribe(m => {
+      this.payments = m;
+    //  console.log(this.payments);
+      this.rowData = m;
+   //   this.kkShowTicketCount();
+      this.checkDetails();
+    });
     
   }
 
-  title = 'app';
+  title = 'Report';
 
-	columnDefs = [
-    { field: 'id', sortable: true, filter: true, width: 90, cellClass: 'id-class center' },
-		{ field: 'firstname', sortable: true, filter: true , cellClass: 'center' },
-		{ field: 'lastname', sortable: true, filter: true, cellClass: 'center' },
-    { field: 'email', sortable: true, filter: true },
-    { field: 'phone', sortable: true, filter: true },
-    { field: 'joined', sortable: true, filter: true},
-    { field: 'expires', sortable: true, filter: true},
-    { field: 'membershipstatus', sortable: true , filter: true},
-    { field: 'address1', sortable: true , filter: true},
-    { field: 'address2', sortable: true , filter: true},
-    { field: 'city', sortable: true , filter: true},
-    { field: 'zipcode', sortable: true , filter: true},
-    { field: 'iAgree', sortable: true , filter: true},
-    { field: 'iAgreeDateTime', sortable: true , filter: true},
-    { field: 'iAgreeIP', sortable: true , filter: true}
-	];
+  checkDetails(){
+    let couter = 0;
+    
 
-
-  onBtnExport() {
-    this.gridApi.exportDataAsCsv();
-  }
-  onGridReady(params) {
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-  }
-
-  kkShowTicketCount(){
-    this.kkHeadCount = 0;
-    this.nonHeadCount = 0;
-    this.vegHeadCount = 0;
-    this.kidHeadCount = 0;
-      [...this.rowData].forEach( xelement =>{
-        [...xelement.pay.transactions[0].item_list.items].forEach(element => {
-          if(element.sku.includes("KK")){
-              this.kkHeadCount += element.quantity ;
-              console.log(element);
-          }
-
-          if(element.sku.includes("NON")){
-              this.nonHeadCount += element.quantity ;
-              console.log(element);
-          }
+  //  console.log(this.rowData);
+    [...this.rowData].forEach( m =>{ 
+    //   console.log(m.purchase? true : false)  ;
+       if(m.purchase? true : false){
+         couter++;
+         [...m.purchase].forEach(element => {
           
-          if(element.sku.includes("VEG")){
-              this.vegHeadCount += element.quantity ;
-              console.log(element);
-          }
-
-          if(element.sku.includes("KID")){
-              this.kidHeadCount += element.quantity ;
-              console.log(element);
-          }
+    //     console.log(element);
+      //    console.log(element.sku);
+          [...element].forEach(e => {
+                if(e.sku.includes("KK")){
+                    this.kkHeadCount += e.quantity ;
+        //            console.log(e.quantity);
+                }
+                if(e.sku.includes("NON")){
+                    this.nonHeadCount += e.quantity ;
+         //           console.log('element.quantity'+e.quantity);
+                }
+                if(e.sku.includes("VEG")){
+                    this.vegHeadCount += e.quantity ;
+                }
+                if(e.sku.includes("KID")){
+                    this.kidHeadCount += e.quantity ;
+                }
+                
+          });
+          
           
         });
-      });
-      console.log(this.kkHeadCount);
+       }
+
+
+    });
+ //   console.log(couter);
+  //  this.lastOrder = this.rowData.purchase? true : false ;
   }
+
+
+  // kkShowTicketCount(){
+  //   this.kkHeadCount = 0;
+  //   this.nonHeadCount = 0;
+  //   this.vegHeadCount = 0;
+  //   this.kidHeadCount = 0;
+  //     [...this.rowData].forEach( xelement =>{ 
+  //       console.log(xelement);
+  //       [...xelement.pay.transactions[0].item_list.items].forEach(element => {
+  //         if(element.sku.includes("KK")){
+  //             this.kkHeadCount += element.quantity ;
+  //             console.log(element);
+  //         }
+
+  //         if(element.sku.includes("NON")){
+  //             this.nonHeadCount += element.quantity ;
+  //             console.log(element);
+  //         }
+          
+  //         if(element.sku.includes("VEG")){
+  //             this.vegHeadCount += element.quantity ;
+  //             console.log(element);
+  //         }
+
+  //         if(element.sku.includes("KID")){
+  //             this.kidHeadCount += element.quantity ;
+  //             console.log(element);
+  //         }
+          
+  //       });
+  //     });
+  //     console.log(this.kkHeadCount);
+  // }
 
 }
